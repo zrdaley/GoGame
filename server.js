3e397ca784b67e2cf47eab3d04e88a97c203c82d
Zenara Daley <Zenara@w87-163-215.wireless.uvic.ca>
2016-07-03 14:58:10 -0700

    moved all items to the public folder

--- server.js
+++ server.js
@@ -3,36 +3,33 @@
 var express    = require("express");
 var bodyParser = require("body-parser");
 
-// The server currently uses an in memory
-// data store. You must implement the required
-// functionality in ./lib/storage.js and 
-// switch out this variable. 
-//var Storage = require('./lib/MongoDB');
-//var Storage = require('./lib/MongoDB');
 
 var app = express();
 
-//var db = new Storage(null, null, 'timer');
 
-// use the parse to get JSON objects out of the request. 
 app.use(bodyParser.json());
 
-// server static files from the public/ directory.
 app.use(express.static('public'));
+
+
+/**
+ * Handle a request for task data.
+ */
+app.get("/index.html", function (req, res) {
+    console.log("GET Request to: /index.html");
+
+
+    
+});
+
 
 /**
  * Handle a request for task data.
  */
 app.get("/data", function (req, res) {
     console.log("GET Request to: /data");
-    
-     /*db.getAllTasks(function(err, data){
-         if(err){
-            res.status(500).send();
-        }else{
-            res.status(200).json(data);
-        } 
-     });*/
+
+
     
 });
 
@@ -43,13 +40,6 @@ app.post("/makePlayer", function (req, res) {
 
     console.log("POST Request to: /makePlayer");
     
-    /*db.addTask(req.body, function(err){
-        if(err){
-            res.status(500).send();
-        }else{
-            res.status(200).send();
-        }
-    });*/
     
     res.status(200).send();
 });
@@ -59,8 +49,5 @@ app.listen(process.env.PORT || 3000, function () {
     
     console.log("Listening on port 3000");
     
-   // db.connect(function(){
-        // some message here....
-   // });
-    
+
 });
